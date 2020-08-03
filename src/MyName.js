b5480bcca7ff4274c2e330a4861290ed4a9891ec
poolpal <zoo5871@naver.com>
2020-08-03 14:23:34 +0900

    2020-08-03

--- src/MyName.js
+++ src/MyName.js
@@ -3,12 +3,25 @@ import React, { Component } from 'react'
 class MyName extends React.Component{
     constructor(props) {
         super(props);
-        
+        this.state={
+            first : Math.ceil(Math.random()*9),
+            second : Math.ceil(Math.random()*9),
+            value : '',
+            result : '',
+        };
     }
     render() {
         return (
             <div>
-                안녕하세요! <b>김주언</b> 입니다!
+               <div>
+                   {this.state.first} 곱하기 {this.state.second}는?
+               </div>
+               <form onSubmit={onsubmit}>
+                    <input type="number" value={this.state.value} onChange={(e)=>this.setState({value : e.target.value})}/>
+                    <button>입력!</button>
+
+               </form>
+               <div>{this.state.result}</div>
             </div>
         )
     }
