357cd0281b0960eb154eab539b4f52d2d9e063ff
poolpal <zoo5871@naver.com>
2020-08-03 14:16:33 +0900

    2020-08-03

---
+++ src/MyName.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class MyName extends React.Component{
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return (
+            <div>
+                안녕하세요! <b>김주언</b> 입니다!
+            </div>
+        )
+    }
+    
+}
+
+export default MyName;
